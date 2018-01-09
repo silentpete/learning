a542f824211d92917e17963763be96b481cba64f
Unknown <peter.gallerani@gmail.com>
2018-01-08 20:43:38 -0700

    proving intellipath lies

---
+++ ctu/it151/intellipath_lies_00.java
@@ -0,0 +1,10 @@
+class intellipath_lies_00 {
+  public static void main(String args[]) {
+    String fname, lname, output;
+    int age = 6;
+    fname = "John";
+    lname = "Smith";
+    output = "Happy Birthday" + fname + ' ' + lname +". You are" + age + "years old today!";
+    System.out.println(output);
+  }
+}
